aa07cf0c4a97e2e71ae5af93092e124249faf3db
Fernando11223 <wlatorrenuse@gmail.com>
2026-05-17 19:34:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def actualizar_dashboard(depto_seleccionado, manera_seleccionada):
     return kpi1, kpi2, kpi3, fig_mapa, fig_edad, fig_lineas, fig_sexo_depto, fig_causas, fig_tabla, fig_ciudades, fig_menor_mort, fig_genero
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
